e6b88ae9013652dfa5a31d46dd8ae1df79e455cb
felixbvf <felixbvf@gmail.com>
2019-10-03 17:24:29 -0400

    proyecto ordenado

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:stateless_app/src/app.dart';
 
 void main() {
   runApp(MaterialApp(
     home: MyApp()
   ));
-}
-
-class MyApp extends StatelessWidget {
-  final double iconSize = 40.0;
-  final TextStyle textStyle = TextStyle(
-                        color: Colors.grey,
-                        fontStyle: FontStyle.normal,
-                        fontSize: 30);
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Stateless widget"),
-      ),
-
-      body: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            MyCard(
-              title: Text("I like Flutter",style: textStyle),
-              icon: Icon(Icons.favorite,color: Colors.redAccent, size: iconSize)
-            ),
-            MyCard(
-              title: Text("I like this video",style: textStyle),
-              icon: Icon(Icons.thumb_up, color: Colors.blueAccent, size: iconSize)
-            ),
-            MyCard(
-              title: Text("Next video",style: textStyle),
-              icon: Icon(Icons.queue_play_next, color: Colors.brown, size: iconSize)
-            )
-          ]
-        ),
-      ),
-    );
-  }
-
-
-}
-
-//
-class MyCard extends StatelessWidget{
-  final Widget title;
-  final Widget icon;
-
-  MyCard({this.title, this.icon});
-
-  @override
-  Widget build(BuildContext context)
-  {
-    return Card (
-              child: Container(
-                padding: EdgeInsets.all(20.0),
-                child: Column(
-                  children: <Widget>[
-                    this.title,
-                    this.icon
-                  ],
-                )
-              )
-    );
-
-  }
-
-
 }
