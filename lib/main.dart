cf9cc46119cd0ed3bcdd5e8238144e910cdee9d6
felixbvf <felixbvf@gmail.com>
2019-10-03 16:39:44 -0400

    widget anidados

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,11 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  final double iconSize = 40.0;
+  final TextStyle textStyle = TextStyle(
+                        color: Colors.grey,
+                        fontStyle: FontStyle.normal,
+                        fontSize: 30);
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -18,33 +23,49 @@ class MyApp extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Card (
-              child: Container(
-                padding: EdgeInsets.all(20.0),
-                child: Column(
-                  children: <Widget>[
-                    Text(
-                      "I like Flutter",
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontStyle: FontStyle.normal,
-                        fontSize: 30
-                      ),
-                    ),
-                    Icon(
-                      Icons.favorite,
-                      color: Colors.redAccent,
-                      size: 40.0,
-                    )
-                  ],
-                )
-              )
+            MyCard(
+              title: Text("I like Flutter",style: textStyle),
+              icon: Icon(Icons.favorite,color: Colors.redAccent, size: iconSize)
+            ),
+            MyCard(
+              title: Text("I like this video",style: textStyle),
+              icon: Icon(Icons.thumb_up, color: Colors.blueAccent, size: iconSize)
+            ),
+            MyCard(
+              title: Text("Next video",style: textStyle),
+              icon: Icon(Icons.queue_play_next, color: Colors.brown, size: iconSize)
             )
           ]
         ),
       ),
-
     );
+  }
+
+
+}
+
+//
+class MyCard extends StatelessWidget{
+  final Widget title;
+  final Widget icon;
+
+  MyCard({this.title, this.icon});
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Card (
+              child: Container(
+                padding: EdgeInsets.all(20.0),
+                child: Column(
+                  children: <Widget>[
+                    this.title,
+                    this.icon
+                  ],
+                )
+              )
+    );
+
   }
 
 
